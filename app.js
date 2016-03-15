1ef7e723ee693906af0ea825d1af43d9024a9079
Brendan Nelson <brendan.nelson49@gmail.com>
2016-03-15 14:53:04 -0700

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-var module = angular.module('app', []);
-
-module.controller('AppCtrl', AppCtrl);
-
-function AppCtrl() {
-    this.message = 'Hello from AngularLand';
-    this.colors = ['red', 'blue', 'green'];
-}
